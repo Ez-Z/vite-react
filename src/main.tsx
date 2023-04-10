1bda25626b820b02219b1696761fcf492071e838
zhoushengda <377210988@qq.com>
2023-04-10 14:39:16 +0800

    style: fastclick

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import { store } from "@/store";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import Routes from "@/Routes";
+import initReactFastclick from "react-fastclick";
 import "./index.css";
+initReactFastclick();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
